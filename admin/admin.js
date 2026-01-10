fd2ca576bbc8efd4f6c2f39f78083f980f65e82e
Bannivasara <make.sortti@gmail.com>
2026-01-10 15:11:27 +0200

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             try {
                 // Lähetetään tiedot palvelimelle
-                const r = await fetch('https://kortti-api.bannivasara.workers.dev/admin-kirjaudu', {
+                const r = await fetch('https://admin.bannivasara.workers.dev/', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ username: user, password: pass })
